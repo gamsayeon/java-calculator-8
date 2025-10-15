89c62963c9abd9001f22a3399e8788059902c1e1
gamsayeon <aud4551@naver.com>
2025-10-15 16:34:39 +0900

    test: 반례 테스트 코드 추가
    - 기본/커스텀 구분자 혼합 덧셈 테스트 추가
    - 기본 구분자 두자리 덧셈 테스트 추가
    - 커스텀 구분자 두자리 덧셈 테스트 추가
    - 기본/커스텀 구분자 혼합 두자리 덧셈 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -17,6 +17,38 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 기본_커스텀_구분자_혼합_덧셈() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;5,2:3");
+            assertThat(output()).contains("결과 : 11");
+        });
+    }
+
+    @Test
+    void 기본_구분자_두자리_덧셈() {
+        assertSimpleTest(() -> {
+            run("1,10:2");
+            assertThat(output()).contains("결과 : 13");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_두자리_덧셈() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;15;5");
+            assertThat(output()).contains("결과 : 21");
+        });
+    }
+
+    @Test
+    void 기본_커스텀_구분자_혼합_두자리_덧셈() {
+        assertSimpleTest(() -> {
+            run("//;\\n2:1;18,5");
+            assertThat(output()).contains("결과 : 26");
+        });
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
