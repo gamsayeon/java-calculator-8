7e7e037254fd22a43744ad0ae93466201c139d8a
gamsayeon <aud4551@naver.com>
2025-10-15 18:21:11 +0900

    feat: 커스텀 구분자 덧셈 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,13 +1,29 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculator {
+    private static String regex = "[,:]";
+
     public static int calculate(String input) {
-        String[] numbers = input.split("[,:]");
+        String extractString = removeDelimiterString(input);
+
+        String[] numbers = extractString.split(regex);
         return Arrays.stream(numbers)
                 .filter(s -> !s.isEmpty())
                 .mapToInt(Integer::parseInt)
                 .sum();
+    }
+
+    public static String removeDelimiterString(String input){
+        Pattern pattern = Pattern.compile("^//(.)\\\\n");
+        Matcher matcher = pattern.matcher(input);
+        if(matcher.find()){
+            regex = regex.replace("]",matcher.group(1)+"]");
+            return matcher.replaceAll("");
+        }
+        return input;
     }
 }
